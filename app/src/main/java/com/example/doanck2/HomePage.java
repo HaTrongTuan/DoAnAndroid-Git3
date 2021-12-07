31df89153e3fb1634efcf15f69f500be9085220c
HaTrongTuan <tuanht19411@st.uel.edu.vn>
2021-12-07 22:48:16 +0700

    First DataBase

--- app/src/main/java/com/example/doanck2/HomePage.java
+++ app/src/main/java/com/example/doanck2/HomePage.java
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePage extends AppCompatActivity {
+
+    TextView txtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,13 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         bottomNav();
+        linkViews();
 
 
+    }
+
+    private void linkViews() {
+        txtName = findViewById(R.id.txtName);
     }
 
     private void bottomNav() {
