6dbfc067aca4914c8d0da0e30a228daa7a752b16
Trongpm19411 <trongpm19411@st.uel.edu.vn>
2021-12-22 18:36:46 +0700

    update premium profile

--- app/src/main/java/com/example/doanck2/Profile_Premium.java
+++ app/src/main/java/com/example/doanck2/Profile_Premium.java
@@ -3,12 +3,27 @@ package com.example.doanck2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class Profile_Premium extends AppCompatActivity {
+    private DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_premium);
+        linkview();
+
+    }
+
+
+
+
+
+
+    private void linkview() {
+        datePicker = findViewById(R.id.datepickerLich);
     }
 }
