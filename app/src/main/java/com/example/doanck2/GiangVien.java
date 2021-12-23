0fb1c08f94ed478c6d2ca9a532cf203197b5a42e
MintTree0811 <93387289+MintTree0811@users.noreply.github.com>
2021-12-24 00:19:23 +0700

    dialog

--- app/src/main/java/com/example/doanck2/GiangVien.java
+++ app/src/main/java/com/example/doanck2/GiangVien.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 public class GiangVien extends AppCompatActivity {
     ImageButton imbBack;
-    RecyclerView rcvTeacher;
+    RecyclerView rcvTeacher, rcvTeacherNormal;
     ItemTeacherAdapter adapter;
 
     @Override
@@ -38,10 +38,24 @@ public class GiangVien extends AppCompatActivity {
         ArrayList<Teacher> demo = new ArrayList<>();
         demo.add(new Teacher(R.drawable.ava,"Hồ Hoàng"));
         demo.add(new Teacher(R.drawable.ava2,"Hoàng Minh"));
-        demo.add(new Teacher(R.drawable.unknownava,"Mỹ Lệ"));
+        demo.add(new Teacher(R.drawable.avapro5,"Mỹ Lệ"));
+        demo.add(new Teacher(R.drawable.avapro,"Văn Minh"));
 
         adapter = new ItemTeacherAdapter(getApplicationContext(), demo);
         rcvTeacher.setAdapter(adapter);
+
+        ArrayList<Teacher> normal = new ArrayList<>();
+        demo.add(new Teacher(R.drawable.hinh4,"Minh Hoàn"));
+        demo.add(new Teacher(R.drawable.hinh1,"Nguyễn Minh Long"));
+        demo.add(new Teacher(R.drawable.hinh3,"Lê Thị Hồng"));
+        demo.add(new Teacher(R.drawable.hinh7,"Văn Minh Hồ"));
+        demo.add(new Teacher(R.drawable.avapro3,"Hồ Minh Nghĩa"));
+        demo.add(new Teacher(R.drawable.hinh3,"Đinh Minh Lộc"));
+        demo.add(new Teacher(R.drawable.gl,"Nguyễn Thị Thu"));
+        demo.add(new Teacher(R.drawable.hinh6,"Hoàng Kim Trinh"));
+
+        adapter = new ItemTeacherAdapter(getApplicationContext(), normal);
+        rcvTeacherNormal.setAdapter(adapter);
     }
 
     private void configRecyclerView() {
@@ -50,6 +64,8 @@ public class GiangVien extends AppCompatActivity {
         DividerItemDecoration decoration = new DividerItemDecoration(this, manager.getOrientation());
         rcvTeacher.addItemDecoration(decoration);
         rcvTeacher.setHasFixedSize(true);
+
+        rcvTeacherNormal.setLayoutManager(manager);
     }
 
     private void back() {
@@ -64,5 +80,6 @@ public class GiangVien extends AppCompatActivity {
     private void linkview() {
         imbBack = findViewById(R.id.imbBack);
         rcvTeacher =findViewById(R.id.rcvTeacher);
+        rcvTeacherNormal = findViewById(R.id.rcvTeacherNormal);
     }
 }
