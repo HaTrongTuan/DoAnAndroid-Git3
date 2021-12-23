308712f0c8638cdea64bc240d729b72202d434e5
Trongpm19411 <thuyenpl19411@st.uel.edu.vn>
2021-12-23 10:51:03 +0700

    update premium payment

--- app/src/main/java/com/example/model/PremiumPayment.java
+++ app/src/main/java/com/example/model/PremiumPayment.java
@@ -5,19 +5,13 @@ import android.widget.TextView;
 
 public class PremiumPayment {
     private String Payment;
-    public boolean expanded;
 
-    public boolean isExpanded() {
-        return expanded;
-    }
 
-    public void setExpanded(boolean expanded) {
-        this.expanded = expanded;
-    }
+
 
     public PremiumPayment(String Payment, boolean expanded) {
         this.Payment = Payment;
-        this.expanded = expanded;
+
     }
 
     public String getPayment() {
